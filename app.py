72be60cd5dde94adc1d141af0d4b425291ac2ea9
IffatK <kiffat679@gmail.com>
2026-03-26 21:37:50 +0530

    runtime

--- app.py
+++ app.py
@@ -1158,5 +1158,6 @@ def feasible_vertices(A, b, x_max, y_max, con_types):
 
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
